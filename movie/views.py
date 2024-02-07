7f34a1e71e371a946bf559be6865524b179c53ff
DJara14 <danijara33@hotmail.com>
2024-02-07 11:35:09 -0500

    cambios home

--- movie/views.py
+++ movie/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request,'home.html',{'name':'Gerg Lim'})
+    return render(request,'home.html',{'name':'Daniel Jaramillo Valencia'})
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
